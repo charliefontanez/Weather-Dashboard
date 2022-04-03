64df325926c7880868a8180919d0401d2c25b758
Carlos Fontanez <charliefontanezis@gmail.com>
2022-04-03 12:36:56 -0400

    restyled forecast box

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,15 @@ for (let i = 0; i < 8; i++) {
 
   secondaryBtn.classList.add("btn-secondary");
   secondaryBtn.classList.add("btn");
-  secondaryBtn.setAttribute("type", "button");
-  secondaryBtn.setAttribute("value", "");
+  secondaryBtn.innerHTML = "city";
+  // secondaryBtn.setAttribute("type", "button");
+  // secondaryBtn.setAttribute("value", "");
   citySearch.append(secondaryBtn);
   // console.log(secondaryBtn);
 }
 
-// fetch('https://api.openweathermap.org/data/2.5/solar_radiation?lat=28.5383&lon=-81.3792&appid=709cd6f20260c1a83a27ae8068f9a762').then(function(data) {
-// data.json();
-// console.log(data);
-// });
+fetch('https://api.openweathermap.org/data/2.5/solar_radiation?lat=28.5383&lon=-81.3792&appid=709cd6f20260c1a83a27ae8068f9a762')
+.then(function(data) {
+  data.json();
+  console.log(data);
+});
